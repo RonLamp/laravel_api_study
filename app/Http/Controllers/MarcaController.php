a27a56b1423668a759f064a36d365989efc4ff99
RonLamp <lamp@rc12.net>
2023-05-02 17:33:56 -0300

    fourth commit POST/PUT/PATCH/GET/DELETE

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -1,56 +1,105 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Models\Marca;
 use Illuminate\Http\Request;
 
 class MarcaController extends Controller
 {
+    public function __construct(Marca $marca){
+        $this->marca = $marca;
+    }
+
     /**
+     * Mostrando a lista de recursos
      * Display a listing of the resource.
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        $marcas = Marca::all();
-        return $marcas;
+        $marcas = $this->marca->all();
+        return response()->json($marcas, 200);
     }
 
-        /**
+    /**
+     * Armazenando um novo recurso
      * Store a newly created resource in storage.
+     * @param \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        $marca = Marca::create($request->all());
-        return $marca;
+        // Validação dos parametros nome e imagem
+        $request->validate($this->marca->rules(), $this->marca->feedback());
+        // Execução da ação propriamente dita
+        $marca = $this->marca->create($request->all());
+        return response()->json($marca, 201);
     }
-
-    /**
+    
+    /** 
+     * Mostrando um recurso específico em função do id
      * Display the specified resource.
+     * @param Integer
+     * @return \Illuminate\Http\Response
      */
-    public function show(Marca $marca)
+    public function show($id)
     {
-        $marcas = Marca::find($marca);
-        return $marcas;
-    }
-
-       /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Marca $marca)
-    {
-        print_r($request->all()); // os dados atualizados
-        echo '<hr>';
-        print_r($marca->getAttributes());  //os dados antigos
-        // $marca = Marca::update()
-        return 'chegamos até aqui update()';
+        $marca = $this->marca->find($id);
+        if($marca===null){
+            return response()
+            ->json(['error'=>'Recurso pesquisado inexistente!'], 404);
+        }
+        return response()->json($marca, 200);
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Atualizando um recurso específico em função do id
+     * Update the specified resource in storage.
+     * @param \Illuminate\Http\Request  $request
+     * @param Integer
+     * @return \Illuminate\Http\Response
      */
-    public function destroy(Marca $marca)
+    public function update(Request $request, $id)
     {
-        //
+        // Localizando o resource (registro)
+        $marca = $this->marca->find($id);
+        if($marca===null){
+            return response()
+            ->json(['error'=>'Atualização indisponível, recurso pesquisado inexistente!'], 404);
+        }
+        // Validação dos parametros nome e imagem
+        //    levando em conta se o método é PUT ou PATCH
+        if($request.method() === 'PATCH'){
+            $dinamycRules = array();
+            // percorrendo todas as regras definidas no Model
+            foreach($marca->rules() as $input => $regra){
+                // coletar somente as regras aplicaveis
+                if(array_key_exists($input, $request->all())){
+                    $dinamycRules[$input] = $regra;
+                }
+            }
+            $request->validate($dinamycRules, $marca->feedback());
+        } else {
+            $request->validate($marca->rules(), $marca->feedback());
+        }
+        // Execução da ação propriamente dita
+        $marca->update($request->all());
+        return response()->json($marca, 200);
+    }
+
+    /**
+     * Deleta ou destroy um recurso específico.
+     * Remove the specified resource from storage.
+     * @param Integer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $marca = $this->marca->find($id);
+        if($marca===null){
+            return response()
+            ->json(['error'=>'Recurso já não existente!'], 404);
+        }
+        $marca->delete($id);
+        return response()->json(['msg'=>'A marca foi removida com sucesso!'], 200);;
     }
 }
--- app/Models/Marca.php
+++ app/Models/Marca.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,4 +7,24 @@ class Marca extends Model
 {
     use HasFactory;
     protected $fillable = ['nome','imagem'];
+
+    public function rules(){
+        return [
+                'nome'=>'required|unique:marcas,nome,'.$this->id.'|min:3',
+                'imagem'=>'required'
+            ];
+        /* Os parametros da pesquisa unique são:
+            1) tabela onde será feita a pesquisa,
+            2) nome da coluna que será feita a pesquisa e
+            3) id do registro que será desconsiderado na pesquisa
+        */
+    }
+
+    public function feedback(){
+        return  [
+                'required' => 'O campo :attribute é obrigatório.',
+                'nome.unique' => 'O nome da marca já existe.',
+                'nome.min' => 'O nome deve ter no mínimo 3 caracteres.'
+            ];
+    }
 }
